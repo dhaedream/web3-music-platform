65849c733e6ce74a84e25df50a294992ae068066
Dhaesha M <dhaesha1@gmail.com>
2022-04-15 00:54:32 -0400

    will need to create and ipfs set up before I continue using json + car.ipfs.io + nft.storage

--- src/backend/contracts/MusicNFTMarketplace.sol
+++ src/backend/contracts/MusicNFTMarketplace.sol
@@ -3,3 +3,9 @@ pragma solidity ^0.7.0;
 
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+contract MusicNFTMarketPlace is ERC721, Ownable {
+    //ipfs from json > car.ipfs.io > nft.storage
+    string public baseUri = ""
+
+}
